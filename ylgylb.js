0a15dab2f65b2da7b80d7e9c91cb70415496fb5e
xinerqu <xinerqu@users.noreply.github.com>
2022-09-19 01:16:53 +0800

    Update ylgylb.js

--- ylgylb.js
+++ ylgylb.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 
-obj.data["daily_count"]= 1e+300;
-obj.data["challenge"]= 1e+300;
+obj.data["daily_count"]= 5201314;
+obj.data["challenge"]= 13145200;
 
 $done({body: JSON.stringify(obj)});
