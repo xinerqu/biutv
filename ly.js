7278dbf2ef9d9e85caab786200aee390147a228a
xinerqu <xinerqu@users.noreply.github.com>
2022-10-11 00:45:57 +0800

    Update and rename films to ly.js

---
+++ ly.js
@@ -0,0 +1,9 @@
+var obj = JSON.parse($response.body);
+delete obj.data.code
+delete obj.data.msg
+delete obj.data.limit
+delete obj.data.pagecount
+delete obj.data. total
+delete obj.data.list
+delete obj.data.vod_id
+$done({body: JSON.stringify(obj)});
