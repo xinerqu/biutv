5e7021be99d73983664453aa5278f8f01f809dbf
xinerqu <xinerqu@users.noreply.github.com>
2022-10-11 02:02:21 +0800

    Update ly.js

--- ly.js
+++ ly.js
@@ -1,11 +1,5 @@
 let body = JSON.parse($response.body)
 if($request.url.indexOf(wd)!=-1){
-delete body.code;
-delete body.msg;
-delete body.limit;
-delete body.pagecount;
-delete body.total;
-delete body.list;
-delete body.vod_id;
+delete body.code
 }
 $done({body: JSON.stringify(body)});
